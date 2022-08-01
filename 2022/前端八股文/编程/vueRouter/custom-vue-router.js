55fc836f6b66e246eadc75564e1cce0eb96629eb
ABoyCDog <178657263@qq.com>
2022-08-01 19:38:44 +0800

    update

--- 2022/前端八股文/编程/vueRouter/custom-vue-router.js
+++ 2022/前端八股文/编程/vueRouter/custom-vue-router.js
@@ -4,8 +4,11 @@ export default class VueRouter {
     constructor({ routes }) {
         this.routes = routes;
         this.history = new History();
+        this.path = window.location.hash; // 注意需要初始化
         this.history.listen((path) => {
+            // 每次变化的path
             this.path = path;
+            this.vm.$forceUpdate();
         });
     }
 
@@ -40,8 +43,24 @@ VueRouter.install = function(Vue) {
             functional: true,
 
             render(createElement, {props, children, parent, data}) {
+
+                const router = parent.$options.router;
+                const path = router.path;
+
+                const matchRoute = router.routes.find(route => {
+                    return route.path.replace(/^\//, '') === path.replace(/\#/, '');
+                })
+
+                const matchedComponent = matchRoute.component;
+
+                // let comp = {
+                //     template: '<div>我是router-view</div>'
+                // };
+
+
                 return createElement(
-                    'div'
+                    // comp,
+                    matchedComponent,
                 );
             }
     })
