c16409d4e724f01d8a386c3210fb69cd9832d517
ABoyCDog <178657263@qq.com>
2022-08-07 14:58:51 +0800

    update

--- 2022/前端八股文/编程/range.js
+++ 2022/前端八股文/编程/range.js
@@ -116,37 +116,35 @@ class RangeList {
 }
 
 // Example run
-const rl = new RangeList();
+// const rl = new RangeList();
 
-rl.add([1, 5]);
-rl.print(); // [1, 5)
+// rl.add([1, 5]);
+// rl.print(); // [1, 5)
 
-rl.add([10, 20]);
-rl.print(); // [1, 5) [10, 20)
+// rl.add([10, 20]);
+// rl.print(); // [1, 5) [10, 20)
 
-rl.add([20, 20]);
-rl.print(); // [1, 5) [10, 20)
+// rl.add([20, 20]);
+// rl.print(); // [1, 5) [10, 20)
 
-rl.add([20, 21]);
-rl.print(); // [1, 5) [10, 21)
+// rl.add([20, 21]);
+// rl.print(); // [1, 5) [10, 21)
 
-rl.add([2, 4]);
-rl.print(); // [1, 5) [10, 21)
+// rl.add([2, 4]);
+// rl.print(); // [1, 5) [10, 21)
 
-rl.add([3, 8]);
-rl.print(); // [1, 8) [10, 21)
+// rl.add([3, 8]);
+// rl.print(); // [1, 8) [10, 21)
 
-rl.remove([10, 10]);
-rl.print(); // [1, 8) [10, 21)
+// rl.remove([10, 10]);
+// rl.print(); // [1, 8) [10, 21)
 
-rl.remove([10, 11]);
-rl.print(); // [1, 8) [11, 21)
+// rl.remove([10, 11]);
+// rl.print(); // [1, 8) [11, 21)
 
-rl.remove([15, 17]);
-rl.print(); // [1, 8) [11, 15) [17, 21)
+// rl.remove([15, 17]);
+// rl.print(); // [1, 8) [11, 15) [17, 21)
 
-rl.remove([3, 19]);
-rl.print(); // [1, 3) [19, 21)
+// rl.remove([3, 19]);
+// rl.print(); // [1, 3) [19, 21)
 
-rl.remove([20, 20]);
-rl.print();
