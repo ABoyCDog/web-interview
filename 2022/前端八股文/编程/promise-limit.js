2d20251cf30ed7050f1a32322cb1005d35b93dbf
ABoyCDog <178657263@qq.com>
2022-08-09 18:33:22 +0800

    promise-limit处理请求并发

--- 2022/前端八股文/编程/promise-limit.js
+++ 2022/前端八股文/编程/promise-limit.js
@@ -1,4 +1,5 @@
 // promise解决并发控制
+// 题目：要求并发数限制执行，且其中每有一个请求完成则去将新的推入栈继续请求，直到全部请求完毕
 
 function limitLoad (urls, handler, limit) {
     const sequence = [].concat(urls)
@@ -10,9 +11,11 @@ function limitLoad (urls, handler, limit) {
         })
     })
 
+    // 注意是每一个完成则推入新的，而不是几个一起请求完再去推入栈，所以使用race去找出第一个完成的在继续入栈执行，而不是all
     let p = Promise.race(promises)
 
     for(let i = 0; i < sequence.length; i++) {
+        // 通过链式调用去处理
         p = p.then((res) => {
             promises[res] = handler(sequence[i]).then(() => {
                 return res
@@ -20,7 +23,6 @@ function limitLoad (urls, handler, limit) {
             return Promise.race(promises)
         })
     }
-
 }
 
 const urls = [{
